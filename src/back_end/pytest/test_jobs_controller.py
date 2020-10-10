a5810a63513c06b931d9bf274172611b342f7ed0
Kaddache <kaddachezakaria@gmail.com>
2020-10-10 12:22:58 +0200

    [add] testing JobsController.add_text_workflow response JSON schema

--- src/back_end/pytest/test_jobs_controller.py
+++ src/back_end/pytest/test_jobs_controller.py
@@ -1,6 +1,7 @@
 # Imports of external libraries
 import requests
 from boddle import boddle
+import jsonschema
 
 # Imports of internal libraries
 import unittest 
@@ -11,7 +12,13 @@ from controllers.jobs_controller import JobsController
 
 class JobsControllerTest(unittest.TestCase):
 
-	""" Test case for testing the 'random' module. """
+	""" Test case for testing the 'JobsController' class. """
+
+	with open(
+			"./pytest/json_schemas/validate_response.json",
+			"r"
+	) as json_file:
+		RESPONSE_SCHEMA = json.load(json_file)
 
 	def setUp(self):
 		""" Initialize the tests. 
@@ -25,11 +32,17 @@ class JobsControllerTest(unittest.TestCase):
 
 		pass
 
-	def test_correct_input_output(self):
-		""" Method for testing the 'random.sample' function. """
+	def test_add_text_workflow_job(self):
+		""" Method for testing the 'JobsController.add_text_workflow_job' function. """
 
-		with open("pytest/curl_request.json", 'r') as f:
-			parameters = json.load(f)
-		with boddle(json=parameters):
+		# Testing with valid request (valid parameters)
+		with open("./pytest/json_schemas/simple_request.json", 'r') as f:
+			valid_parameters = json.load(f)
+		with boddle(json=valid_parameters):
 			result = JobsController.add_text_workflow_job()
+			# Test type of the response (should be string)
 			self.assertEqual(type(result), str)
+			# Test JSON schema of the response
+			self.assertEqual(jsonschema.validate(json.loads(result), JobsControllerTest.RESPONSE_SCHEMA), None)
+
+
